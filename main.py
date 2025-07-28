6594659f8d038a497f14b1c5e54be24792f67828
Alexkiller3008 <yenussovalex@icloud.com>
2025-07-28 20:56:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное"
-            }
-word = input("Введите непонятное слово (большими буквами!): ")
-if word in meme_dict.keys():
-    print("Что-то очень странное или стыдное")
-else:
-     print("Что-то очень смешное")
+import telebot
+    
+    # Замени 'TOKEN' на токен твоего бота
+    # Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot("7889285425:AAFLKIPxVqAermAW3Y522hn-ZcXVSWZHjzw")
+    
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+     bot.reply_to(message, "Привет! Я твой Telegram бот. я буду подсказывать тебе рецепты!")
+    
+    
+@bot.message_handler(commands=['eggs'])
+def send_bye(message):
+        bot.reply_to(message,  ['разагреваем сковородку ложим туда чють чють масла и разбиваем яица солим и ждём 8 мин после того как они пожарились ложим их на тарелку и наслаждаемся  '])
+    
+@bot.message_handler(commands=['sausages'])
+def send_bye(message):
+        bot.reply_to(message,  ['разрезаем сосиськи и разагреваем сковородку ложим чють чють масла и ложим сосиськи поджариваем до корочки и ложим их на торелку и наслаждаемся  '])
+                    
+@bot.message_handler(commands=['sandwiches'])
+def send_bye(message):
+        bot.reply_to(message,  ['нарезаем хлеб колбасу и сыр и ложим колбасу с сыром на хлеб'])
+                     
+@bot.message_handler(commands=['cheese'])
+def send_bye(message):
+        bot.reply_to(message,  ['нарезаем сосиськи разагреваем сковородку ложим чють чють масла и ложим туда сосиськи жарим их и добовляем кетчюп с мойнезом перемешеваем м добавляем сыр чють чють поджариваем пока сыр не растопится и ложим их на торелку и наслаждаемся'])
+                    
+
+
+
